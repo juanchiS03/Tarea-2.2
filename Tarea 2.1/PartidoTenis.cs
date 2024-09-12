de18488ba92a0ef19ed0c356948ba0dc8f31589c
Juan Sanchez <juansanchezdecorta@gmail.com>
2024-09-12 15:47:35 +0200

    Agregar archivos de proyecto.

---
+++ Tarea 2.1/PartidoTenis.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_2._1
+{
+    internal class PartidoTenis
+    {
+        // Torneo
+        public string tourney_id { get; set; }
+        public string tourney_name { get; set; }
+        public string surface { get; set; }
+        public int? draw_size { get; set; }
+        public string tourney_level { get; set; }
+        public string tourney_date { get; set; }
+
+        // Partido
+        public int match_num { get; set; }
+
+        // Ganador
+        public int? winner_id { get; set; }
+        public int? winner_seed { get; set; }
+        public string winner_entry { get; set; }
+        public string winner_name { get; set; }
+        public string winner_hand { get; set; }
+        public int? winner_ht { get; set; }
+        public string winner_ioc { get; set; }
+        public double winner_age { get; set; }
+
+        // Perdedor
+        public int? loser_id { get; set; }
+        public int? loser_seed { get; set; }
+        public string loser_entry { get; set; }
+        public string loser_name { get; set; }
+        public string loser_hand { get; set; }
+        public int? loser_ht { get; set; }
+        public string loser_ioc { get; set; }
+        public double loser_age { get; set; }
+
+        // Otros detalles del partido
+        public string score { get; set; }
+        public int? best_of { get; set; }
+        public string round { get; set; }
+        public int? minutes { get; set; }
+
+        // Estadísticas del ganador
+        public int? w_ace { get; set; }
+        public int? w_df { get; set; }
+        public int? w_svpt { get; set; }
+        public int? w_1stIn { get; set; }
+        public int? w_1stWon { get; set; }
+        public int? w_2ndWon { get; set; }
+        public int? w_SvGms { get; set; }
+        public int? w_bpSaved { get; set; }
+        public int? w_bpFaced { get; set; }
+
+        // Estadísticas del perdedor
+        public int? l_ace { get; set; }
+        public int? l_df { get; set; }
+        public int? l_svpt { get; set; }
+        public int? l_1stIn { get; set; }
+        public int? l_1stWon { get; set; }
+        public int? l_2ndWon { get; set; }
+        public int? l_SvGms { get; set; }
+        public int? l_bpSaved { get; set; }
+        public int? l_bpFaced { get; set; }
+
+        // Ranking
+        public int? winner_rank { get; set; }
+        public int? winner_rank_points { get; set; }
+        public int? loser_rank { get; set; }
+        public int? loser_rank_points { get; set; }
+
+    }
+}
---
+++ Tarea 2.1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Ruta del archivo CSV (las barras invertidas necesitan ser escapadas)
+using CsvHelper.Configuration;
+using CsvHelper;
+using System.Globalization;
+using Tarea_2._1;
+
+// Ruta del archivo CSV de entrada
+string filePath = @"C:\Users\juansanchez\Downloads\atp_matches_2015.csv";
+
+// Configuración del lector CSV
+var csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+{
+Delimiter = ",",
+HasHeaderRecord = true,
+};
+
+// Se lee el archivo CSV mediante un reader.
+using (var reader = new StreamReader(filePath))
+using (var csv = new CsvReader(reader, csvConfig))
+{
+    // Variable que almacene todos los partidos leídos anteriormente.
+    var partidos = csv.GetRecords<PartidoTenis>().ToList();
+
+    // Mostrar el contenido por consola con tabulaciones como delimitador
+    foreach (var partido in partidos)
+    {
+        Console.WriteLine($"{partido.tourney_id}\t{partido.tourney_name}\t{partido.surface}\t{partido.draw_size}\t" +
+              $"{partido.tourney_level}\t{DateTime.ParseExact(partido.tourney_date, "yyyyMMdd", CultureInfo.InvariantCulture).ToString("yyyy/MM/dd")}\t{partido.match_num}\t" +
+              $"{partido.winner_id}\t{(partido.winner_seed.HasValue ? partido.winner_seed.ToString() : "")}\t" +
+              $"{partido.winner_entry}\t{partido.winner_name}\t{partido.winner_hand}\t{partido.winner_ht}\t" +
+              $"{partido.winner_ioc}\t{partido.winner_age}\t{partido.loser_id}\t" +
+              $"{(partido.loser_seed.HasValue ? partido.loser_seed.ToString() : "")}\t{partido.loser_entry}\t" +
+              $"{partido.loser_name}\t{partido.loser_hand}\t{partido.loser_ht}\t{partido.loser_ioc}\t" +
+              $"{partido.loser_age}\t{partido.score}\t{partido.best_of}\t{partido.round}\t{partido.minutes}\t" +
+              $"{partido.w_ace}\t{partido.w_df}\t{partido.w_svpt}\t{partido.w_1stIn}\t{partido.w_1stWon}\t" +
+              $"{partido.w_2ndWon}\t{partido.w_SvGms}\t{partido.w_bpSaved}\t{partido.w_bpFaced}\t" +
+              $"{partido.l_ace}\t{partido.l_df}\t{partido.l_svpt}\t{partido.l_1stIn}\t{partido.l_1stWon}\t" +
+              $"{partido.l_2ndWon}\t{partido.l_SvGms}\t{partido.l_bpSaved}\t{partido.l_bpFaced}\t" +
+              $"{partido.winner_rank}\t{partido.winner_rank_points}\t{partido.loser_rank}\t{partido.loser_rank_points}");
+    }
+}
